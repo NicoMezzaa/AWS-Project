83d2a8a9c278a5f726396a55157011964fd2f494
Nicolò <nico.mezza7@gmail.com>
2024-05-09 22:35:36 +0200

    u

--- index.php
+++ index.php
@@ -5,14 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="/asset/css/style_index.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
     <div id="login-box">
         <h2>Login</h2>
         <form method="post" action="/includes/login.php">
             <input type="text" name="username" maxlength="30" placeholder="Username" required>
-            <input type="password" name="password" class="input-field" minlength="8" maxlength="16" placeholder="Password" required>
+            <input type="password" name="password" minlength="8" maxlength="16" placeholder="Password" required>
             <button type="submit">Login</button>
         </form>
     </div>
