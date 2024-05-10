dace035c175c75f03aa0d982f4f333832f5a5d75
Nicolò <nico.mezza7@gmail.com>
2024-05-10 23:59:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 session_start();
 
-if ($_SERVER['HTTPS'] !== 'on') {
-    // reindirizza alla stessa URL ma con HTTPS
-    $redirect_url = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-    header("Location: $redirect_url");
-    exit();
-}
 ?>
 
 <!DOCTYPE html>
