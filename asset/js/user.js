b77af93a875b8f4f2096f5de17b56f6ff1cbd599
Nicolò <nico.mezza7@gmail.com>
2024-05-10 23:21:12 +0200

    Update user.js

--- asset/js/user.js
+++ asset/js/user.js
@@ -1,25 +1,31 @@
+// Funzione per ottenere lo username dallato PHP e scriverlo nell'elemento span
 function getUsername() {
     const usernameElement = document.getElementById('username');
     const username = usernameElement.textContent;
 
-    function typeWriter(textElement, text, speed) {
-        let i = 0;
-        const interval = setInterval(() => {
-            textElement.textContent += text.charAt(i);
-            i++;
-            if (i > text.length) {
-                clearInterval(interval);
-                textElement.style.animation = 'none';
-            }
-        }, speed);
+    if (username === '') { // Controllo se lo username è vuoto
+        const sessionUsername = "<?php echo isset($_SESSION['username']) ? $_SESSION['username'] : 'Username'; ?>";
+        typeWriter(usernameElement, sessionUsername, 180);
     }
-
-    typeWriter(usernameElement, username, 180);
 }
 
+// Funzione per animare la scrittura del testo
+function typeWriter(textElement, text, speed) {
+    let i = 0;
+    const interval = setInterval(() => {
+        textElement.textContent += text.charAt(i);
+        i++;
+        if (i > text.length) {
+            clearInterval(interval);
+            textElement.style.animation = 'none';
+        }
+    }, speed);
+}
+
+// Funzione per attivare/disattivare la modalità scura
 function switchTheme() {
     const body = document.body;
-    const loginBox = document.querySelector(".container");
+    const container = document.querySelector(".container");
     const h1 = document.querySelector("h1");
     const inputs = document.querySelectorAll("input");
     const loginButton = document.getElementById("login-button");
@@ -28,19 +34,20 @@ function switchTheme() {
     const a = document.querySelector("a");
 
     body.classList.toggle("dark-mode");
-    p.classList.toggle("dark-mode");
-    a.classList.toggle("dark-mode");
-    loginBox.classList.toggle("dark-mode");
+    container.classList.toggle("dark-mode");
     h1.classList.toggle("dark-mode");
     inputs.forEach(input => {
         input.classList.toggle("dark-mode");
     });
     loginButton.classList.toggle("dark-mode");
+    p.classList.toggle("dark-mode");
+    a.classList.toggle("dark-mode");
     if (h2) { // Controllo se h2 esiste
         h2.classList.toggle("dark-mode");
     }
 }
 
+// Carica la funzione getUsername() quando la pagina è completamente caricata
 window.onload = function() {
     getUsername();
 };
